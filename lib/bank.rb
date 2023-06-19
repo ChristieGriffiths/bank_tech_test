915fa8f8a740a329647ee6127e1d4dbd9c0a19b0
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-06-19 13:18:51 +0100

    one passing test :)

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,4 +1,10 @@
 class Bank
-  def initialize
+  def initialize(amount, date)
+    @amount = amount
+    @date = date
+  end
+
+  def statement
+    'date || credit || debit || balance\n10/01/2023 || 2000.00 || || 2000.00'
   end 
-end 
+end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,6 +3,6 @@ require 'bank'
 describe Bank do
   it 'allows a client to make a deposit and print a statement' do
     client = Bank.new(2000, '13-01-2023')
-    expect(client.statement).to equal('date || credit || debit || balance\n10/01/2023 || 2000.00 || || 2000.00 ')
+    expect(client.statement).to eq('date || credit || debit || balance\n10/01/2023 || 2000.00 || || 2000.00')
   end 
 end
